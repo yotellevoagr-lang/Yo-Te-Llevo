35010e1a1ae2df6b01ac0bc23c76cdbf0f319ab7
PelisCristianas <peliscristianas2001@gmail.com>
2025-11-05 02:39:34 +0000

    me encanto, pero si haces las letras de color negro? es que no puedo ver

--- src/components/searchable-select.tsx
+++ src/components/searchable-select.tsx
@@ -108,7 +108,7 @@ export function SearchableSelect({ options, value, onChange, placeholder, listHe
                                 <div
                                     key={option.value}
                                     className={cn(
-                                        "px-3 py-2 cursor-pointer hover:bg-accent",
+                                        "px-3 py-2 cursor-pointer hover:bg-accent text-foreground",
                                         value === option.value && "bg-accent"
                                     )}
                                     onMouseDown={(e) => { 
