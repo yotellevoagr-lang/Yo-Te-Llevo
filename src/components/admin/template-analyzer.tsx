71962d321ce67e44342f9c7d3a4f8a6073e2c8c2
PelisCristianas <peliscristianas2001@gmail.com>
2025-11-06 22:08:37 +0000

    aveces siento que hablo en chino, no puede revisar por favor y de una ve

--- src/components/admin/template-analyzer.tsx
+++ src/components/admin/template-analyzer.tsx
@@ -127,7 +127,7 @@ export function TemplateAnalyzer({ isOpen, onOpenChange }: TemplateAnalyzerProps
                 
                 <div className="space-y-2">
                     <h4 className="font-semibold flex items-center gap-2"><Users className="w-4 h-4"/> Muestra de Datos de Pasajeros (primeras 5 filas)</h4>
-                    <div className="border rounded-md">
+                    <div className="border rounded-md overflow-x-auto">
                         <Table>
                             <TableHeader><TableRow>{Object.keys(analysisResult.headers).map(h => <TableHead key={h} className="text-xs p-1 whitespace-nowrap">{h}</TableHead>)}</TableRow></TableHeader>
                             <TableBody>
