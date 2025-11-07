8b6ec34ae0626bdeaf5ad592a1d1d5bcade7e106
PelisCristianas <peliscristianas2001@gmail.com>
2025-11-07 21:33:19 +0000

    que los cuadros de seleccion tengan un scroll y no se salgan del cuadro

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -6,6 +6,7 @@ import * as SelectPrimitive from "@radix-ui/react-select"
 import { Check, ChevronDown, ChevronUp } from "lucide-react"
 
 import { cn } from "@/lib/utils"
+import { ScrollArea } from "./scroll-area"
 
 const Select = SelectPrimitive.Root
 
@@ -84,15 +85,15 @@ const SelectContent = React.forwardRef<
     {...props}
   >
     <SelectScrollUpButton />
-    <SelectPrimitive.Viewport
+    <ScrollArea
       className={cn(
-        "p-1 overflow-y-auto",
+        "p-1",
         position === "popper" &&
           "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]"
       )}
     >
       {children}
-    </SelectPrimitive.Viewport>
+    </ScrollArea>
     <SelectScrollDownButton />
   </SelectPrimitive.Content>
 ))
