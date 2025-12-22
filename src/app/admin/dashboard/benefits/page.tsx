2133cbc36892129e286fdc6766c317f93b362a94
yotellevoagr-lang <yotellevoagr@gmail.com>
2025-12-22 07:39:13 +0000

    porque no puedo entrar a la seccion d beneficios?

--- src/app/admin/dashboard/benefits/page.tsx
+++ src/app/admin/dashboard/benefits/page.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useMemo } from 'react';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
@@ -880,4 +880,3 @@ export default function AdminBenefitsPage() {
     </div>
   );
 }
-
